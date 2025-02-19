5e2c4faa76c175c0efe05ccf8dc317c45781ac22
Tadeo Gaviria <tadeogaviria@gmail.com>
2025-02-19 09:28:23 -0500

    creacion de codigo despues del TDD

--- TareaTdd/fizzbuzzTest.cs
+++ TareaTdd/fizzbuzzTest.cs
@@ -46,5 +46,15 @@
 
     }
 
-   
+    internal class FizzBuzz
+    {
+        public static string GetFizzBuzzString(int input)
+        {
+            if (input % 3 == 0 && input % 5 == 0) return "FizzBuzz";
+            if (input % 3 == 0) return "Fizz";
+            if (input % 5 == 0) return "Buzz";
+            return input.ToString();
+        }
+
+    }
 }
--- TareaTdd/obj/Debug/net9.0/TareaTdd.test.AssemblyInfo.cs
+++ TareaTdd/obj/Debug/net9.0/TareaTdd.test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TareaTdd.test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95901e0bc7c8fc8e65695cef859beb1f50a749e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TareaTdd.test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TareaTdd.test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
