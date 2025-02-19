95901e0bc7c8fc8e65695cef859beb1f50a749e8
Tadeo Gaviria <tadeogaviria@gmail.com>
2025-02-19 09:24:39 -0500

    TDD code FizzBuzz

---
+++ TareaTdd/fizzbuzzTest.cs
@@ -0,0 +1,50 @@
+﻿namespace TareaTdd
+{
+    public class fizzbuzzTest
+    {
+    [Theory]
+    [InlineData(1,"1")]
+    [InlineData(2,"2")]
+
+    public void NumbersWithNoSpecialPropertiesReturnTheNumber(int input, string expectedResult)
+         {
+         var actualResult = FizzBuzz.GetFizzBuzzString(input);
+         Assert.Equal(expectedResult, actualResult);
+         }
+
+
+    [Theory]
+    [InlineData(3)]
+    [InlineData(6)]
+    
+
+        public void NumbersDivisibleByThreeButNotFiveReturnFizz(int input)
+        {
+            var actualResult = FizzBuzz.GetFizzBuzzString(input);
+            Assert.Equal("Fizz", actualResult);
+        }
+
+    [Theory]
+    [InlineData(5)]
+    [InlineData(10)]
+
+    public void NumbersDivisibleByFiveButNotThreeReturnBuzz(int input)
+        {
+            var actualResult = FizzBuzz.GetFizzBuzzString(input);
+            Assert.Equal("Buzz", actualResult);
+        }
+
+    [Theory]
+    [InlineData(15)]
+    [InlineData(30)]
+
+    public void NumbersDivisibleByThreeAndFiveReturnFizzBuzz(int input)
+        {
+            var actualResult = FizzBuzz.GetFizzBuzzString(input);
+            Assert.Equal("FizzBuzz", actualResult);
+        }
+
+    }
+
+   
+}
